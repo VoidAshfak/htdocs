cd5cd9448c536cc3cf81e19a4a6c0c827feca51e
VoidAshfak <1703103@student.ruet.ac.bd>
2022-03-26 21:23:03 +0600

    second try

--- userinformation.php
+++ userinformation.php
@@ -1,16 +1,19 @@
 <?php
+session_start();
+//initializing variables
+$user = "";
+$pass = "";
+//connect to database
+$db = mysqli_connect('localhost:8012','root','','contact_info') or die("Couldn't connect to database");
+$user = mysqli_real_escape_string($db,$_POST['user']);
+$email = mysqli_real_escape_string($db,$_POST['email']);
+$message = mysqli_real_escape_string($db,$_POST['message']);
 
-$connection = mysqli_connect('localhost:8012','root');
-mysql_select_db($connection,"contact_data");
+//save data
 
-$user = $_POST['user'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+$query = "INSERT INTO userinfodata (user,email,message) VALUES ('$user','$email','$message')";
+mysqli_query($db,$query);
 
-$query = "INSERT INTO `userinfodata`(`user`,`email`,`message`) VALUES ('$user','$email','$message')";
-
-mysql_query($connection,$query);
-
-echo "Message is sent.";
+echo "Done!";
 
 ?>
